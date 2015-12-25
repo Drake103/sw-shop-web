a29e80017b76dda614de1bfa9156c436535b66d2
drake103 <agabden@gmail.com>
2015-12-25 23:18:42 +0600

    Basic rating is implemented.

--- app/shared/rating/rating.directive.js
+++ app/shared/rating/rating.directive.js
@@ -3,12 +3,34 @@ class Rating {
     this.restrict = 'A';
     this.templateUrl = '/shared/rating/rating.view.html';
     this.scope = {
-      rating: '=dataRating',
+      rating: '=ratingValue',
     };
   }
 
   link(scope, element, attrs, ctrl) {
+    const MAX_RATING = 5;
+    const MIN_RATING = 0;
 
+    let rating = Math.min(scope.rating, MAX_RATING);
+    rating = Math.max(rating, MIN_RATING);
+    let fullStars = Math.floor(rating);
+    let hasHalfStars = rating != fullStars;
+    let emptyStars = Math.floor(MAX_RATING - rating);
+
+    let stars = [];
+    for (let i = 0; i < fullStars; i++) {
+      stars.push({type: 'full'});
+    }
+
+    if (hasHalfStars) {
+      stars.push({type: 'half'});
+    }
+
+    for (let i = 0; i < emptyStars; i++) {
+      stars.push({type: 'empty'});
+    }
+
+    scope.stars = stars;
   }
 
   static directiveFactory() {
